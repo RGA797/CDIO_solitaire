3cafe22c29763b605ade4c7cdbbea822a953bea7
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-03-09 17:07:41 +0100

    implemented unit test for rule 1 and 2

--- app/src/main/java/com/example/cdio_solitaire/MainActivity.kt
+++ app/src/main/java/com/example/cdio_solitaire/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.cdio_solitaire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cdio_solitaire.controller.SolitaireSolver
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("hello world")
     }
 }
--- app/src/test/java/com/example/cdio_solitaire/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cdio_solitaire/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.cdio_solitaire
 
+import com.example.cdio_solitaire.controller.SolitaireSolver
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -14,4 +15,14 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun rule_one_two(){
+        var solitaireSolver = SolitaireSolver()
+        val solution = solitaireSolver.solve()
+        if (solution != null){
+        println("solution!: " + solution[0].rank + solution[0].suit + ", " + solution[1].rank + solution[1].suit )
+        }
+    }
+
+
 }
