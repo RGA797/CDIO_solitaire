63dd2ab789e9f081e97415e050621f9d1587438a
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-03-30 12:37:34 +0200

    removed some redundant code

--- app/src/main/java/com/example/cdio_solitaire/Model/Card.kt
+++ app/src/main/java/com/example/cdio_solitaire/Model/Card.kt
@@ -5,14 +5,6 @@ Rank : 1 = A, 2..10 = 2..10 , 11 = J, 12 = Q, 13 = K
 Suit : D = Diamond, H = Heart, C = Club, S = Spade
  */
 class Card(var rank: Int, var suit: String, var isDowncard: Boolean) {
-    @JvmName("getRank1")
-    fun getRank(): Int {
-        return rank
-    }
-    @JvmName("getSuit1")
-    fun getSuit(): String {
-        return suit
-    }
     fun flipCard(){
         isDowncard= !isDowncard
     }
