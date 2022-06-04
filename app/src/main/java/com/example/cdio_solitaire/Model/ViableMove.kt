41fd9e93672ed52990cf90dd8f1c34ce9ff883e6
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-06-04 03:00:44 +0200

    the solitaire solver finally prints which empty column you can send aces or kings to!

--- app/src/main/java/com/example/cdio_solitaire/Model/ViableMove.kt
+++
@@ -1,4 +0,0 @@
-package com.example.cdio_solitaire.Model
-//data class holding the cards moving from/to and wether it is moved to the bottom column (is bottom solution)
-class ViableMove(val cards: MutableList<Card?>?, isBottomSolution: Boolean) {
-}
